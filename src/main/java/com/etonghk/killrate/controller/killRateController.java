7bf10c33c8366590dead66a8327aca6abe982a96
PeterHong <peter5645@hotmail.com>
2019-01-09 10:08:12 +0800

    新添加 thymeleaf

---
+++ src/main/java/com/etonghk/killrate/controller/killRateController.java
@@ -0,0 +1,16 @@
+package com.etonghk.killrate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class killRateController {
+
+	@RequestMapping("/")
+	public String test(Model model) {
+		
+		model.addAttribute("msg","安安你好");
+		return "/killRate/index";
+	}
+}
