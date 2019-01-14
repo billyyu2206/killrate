3631bfe4fd73f7699eb902ced680318ca0ac1b2a
billy.yu <billy800503@hotmail.com>
2019-01-14 17:27:04 +0800

    補import

--- src/main/java/com/etonghk/killrate/awardNmber/ssc/wuxi/WxhzdxdsAwardNumber.java
+++ src/main/java/com/etonghk/killrate/awardNmber/ssc/wuxi/WxhzdxdsAwardNumber.java
@@ -2,6 +2,7 @@ package com.etonghk.killrate.awardNmber.ssc.wuxi;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
