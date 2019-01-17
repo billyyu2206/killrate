42517925876df3f07545343408166f6e5288a6ee
billy.yu <billy800503@hotmail.com>
2019-01-17 16:05:41 +0800

    補誤刪import

--- src/main/java/com/etonghk/killrate/config/WebMvcConfig.java
+++ src/main/java/com/etonghk/killrate/config/WebMvcConfig.java
@@ -1,21 +1,15 @@
 package com.etonghk.killrate.config;
 
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.etonghk.killrate.config.formatter.DateFormatter;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.etonghk.killrate.interceptor.LoginInterceptor;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -24,10 +18,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		.addPathPatterns("/**")
 		;
 	}
-
+	
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addFormatter(new DateFormatter());
 	}
-
 }
