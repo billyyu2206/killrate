1701d9b725412510ab4cb5ba82228ff967fc9875
PeterHong <peter5645@hotmail.com>
2019-01-17 09:48:36 +0800

    log設定

--- src/main/java/com/etonghk/killrate/controller/LoginContorller.java
+++ src/main/java/com/etonghk/killrate/controller/LoginContorller.java
@@ -3,11 +3,14 @@ package com.etonghk.killrate.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.etonghk.killrate.domain.Account;
 import com.etonghk.killrate.service.account.AccountService;
@@ -15,12 +18,14 @@ import com.etonghk.killrate.service.account.AccountService;
 @Controller
 public class LoginContorller {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private AccountService accountService;
 
 	@GetMapping("/")
 	public String index(Account account) {
-
+		logger.info("123");
 		return "login";
 	}
 
@@ -31,7 +36,6 @@ public class LoginContorller {
 		try {
 			model.addAttribute("errorMsg", null);
 			String ip = getFisrtRemoteHost(request);
-			System.out.println("ip : "+ip);
 			account = accountService.login(account.getAccount(), account.getPassword(),ip);
 			session.setAttribute(session.getId(), account);
 		} catch (RuntimeException e) {
@@ -74,5 +78,11 @@ public class LoginContorller {
 	    
 	    return ip.equals("0:0:0:0:0:0:0:1")?"127.0.0.1":ip;
 	}//method 
+	
+	@RequestMapping("logOut")
+	public String logOut(HttpSession session) {
+		session.removeAttribute(session.getId());
+		return "redirect:/";
+	}
 
 }
