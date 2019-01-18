c9681ed0b281c69537747592f637d5ac10670aa0
ami.tsai <amitsai@etonghk.com>
2019-01-18 16:11:45 +0800

    修正送mq

--- src/main/java/com/etonghk/killrate/controller/BetRecordReceiveController.java
+++ src/main/java/com/etonghk/killrate/controller/BetRecordReceiveController.java
@@ -2,23 +2,31 @@ package com.etonghk.killrate.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.etonghk.killrate.controller.dto.ApiResult;
 import com.etonghk.killrate.controller.dto.request.GameLotteryOrder;
+import com.etonghk.killrate.mq.sender.KillRateBetSender;
 
 @RestController
 @RequestMapping("/betRecord")
 public class BetRecordReceiveController {
 	
+	@Autowired
+	private KillRateBetSender killRateBetSender;
+	
 	@RequestMapping("/send")
-	public ApiResult<Void> betReceive(@RequestBody List<GameLotteryOrder> betRecords) {
+	public ApiResult<Void> betReceive(@RequestBody List<GameLotteryOrder> orders) {
 		ApiResult<Void> result = new ApiResult<Void>();
+
+		orders.forEach(order->{
+			killRateBetSender.senderGameLotteryOrder(order);
+		});
 		
 		// TODO send MQ
-		
 		result.setCode(ApiResult.SUCCESS_CODE);
 		result.setMsg("success");
 		return result;
