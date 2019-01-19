cd27eee2579502cb3aeaae3df35a12f63e7ccf86
billy.yu <billy800503@hotmail.com>
2019-01-19 17:22:13 +0800

    修正無法注入jar檔 Component

--- src/main/java/com/etonghk/killrate/KillRateApplication.java
+++ src/main/java/com/etonghk/killrate/KillRateApplication.java
@@ -3,10 +3,13 @@ package com.etonghk.killrate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.jack.pool.impl.DataFactoryImpl;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.etonghk.killrate.dao")
+@Import(DataFactoryImpl.class)
 public class KillRateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KillRateApplication.class, args);
