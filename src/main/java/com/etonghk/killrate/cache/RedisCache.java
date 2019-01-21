4f3ac93df063921466c155c00667eb42d5027aaa
billy.yu <billy800503@hotmail.com>
2019-01-21 10:43:27 +0800

    移除多餘import

--- src/main/java/com/etonghk/killrate/cache/RedisCache.java
+++ src/main/java/com/etonghk/killrate/cache/RedisCache.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
 
