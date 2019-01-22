7adc9824750fea882745127a0213feed1277a7ad
billy.yu <billy800503@hotmail.com>
2019-01-23 00:19:34 +0800

    移除import

--- src/main/java/com/etonghk/killrate/awardNmber/ssc/wuxi/Wxzux30AwardNumber.java
+++ src/main/java/com/etonghk/killrate/awardNmber/ssc/wuxi/Wxzux30AwardNumber.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Component;
 
 import com.etonghk.killrate.anootations.AwardComponent;
 import com.etonghk.killrate.awardNmber.AwardNumber;
