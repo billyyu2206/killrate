8aa7ecca72d34b29419e7259c4cb4bfdd1db19aa
PeterHong <peter5645@hotmail.com>
2019-01-22 17:27:54 +0800

    跨度 混合組選 定位膽 直選複式

---
+++ src/test/java/com/etonghk/killrate/awardNumber/yixi/TestDwAwardNumber.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.etonghk.killrate.awardNumber.yixi;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.etonghk.killrate.awardNumber.TestAwardNumberBasic;
+
+/**
+ * 	一星_定位胆 dw
+ * @author Peter.Hong
+ * @date 2019年1月22日
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDwAwardNumber extends TestAwardNumberBasic{
+
+	@Before
+	public void beforeTest() {
+		order.setContent("12,-,5,-,-");
+		order.setMethod("dw");
+		order.setLottery("vipssc");
+		order.setMultiple(100);
+		order.setMoney(1);
+		order.setModel("yuan");
+		order.setIssue("201901191002");
+	}
+}
