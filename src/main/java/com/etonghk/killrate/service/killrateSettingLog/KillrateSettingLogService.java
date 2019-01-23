446a971b44dd7990f4d4f57db512cca06ea1f7fc
billy.yu <billy800503@hotmail.com>
2019-01-23 17:50:17 +0800

    新增殺率設定紀錄查詢頁面

---
+++ src/main/java/com/etonghk/killrate/service/killrateSettingLog/KillrateSettingLogService.java
@@ -0,0 +1,13 @@
+package com.etonghk.killrate.service.killrateSettingLog;
+
+import com.etonghk.killrate.controller.dto.request.KillrateSettingLogRequest;
+import com.etonghk.killrate.dao.page.Page;
+import com.etonghk.killrate.domain.KillrateSettingLog;
+
+/**
+ * @author Billy.Yu
+ * @date 2019年1月23日
+ */
+public interface KillrateSettingLogService {
+	public Page<KillrateSettingLog> selectForPage(KillrateSettingLogRequest cond, Page<KillrateSettingLog> page);
+}
