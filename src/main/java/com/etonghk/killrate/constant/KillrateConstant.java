2ad46afa258ff093251fdddc67099e2e9101cab3
PeterHong <peter5645@hotmail.com>
2019-01-23 17:20:14 +0800

    gameId修正

--- src/main/java/com/etonghk/killrate/constant/KillrateConstant.java
+++ src/main/java/com/etonghk/killrate/constant/KillrateConstant.java
@@ -16,13 +16,13 @@ public class KillrateConstant {
 	public static Map<String, String> allGameMap;
 	static {
 		allGameMap = new HashMap<String, String>();
-		allGameMap.put("tls60","tls60");
-		allGameMap.put("tls90","tls90");
-		allGameMap.put("tls30","tls30");
-		allGameMap.put("tls90a","tls90a");
-		allGameMap.put("tls90b","tls90b");
-		allGameMap.put("tls90c","tls90c");
-		allGameMap.put("tls90d","tls90d");
+		allGameMap.put("t1s60","t1s60");
+		allGameMap.put("t1s90","t1s90");
+		allGameMap.put("t1s30","t1s30");
+		allGameMap.put("t1s90a","t1s90a");
+		allGameMap.put("t1s90b","t1s90b");
+		allGameMap.put("t1s90c","t1s90c");
+		allGameMap.put("t1s90d","t1s90d");
 		allGameMap.put("t2s30","t2s30");
 		allGameMap.put("t3s90","t3s90");
 		allGameMap.put("t2s90","t2s90");
@@ -34,13 +34,13 @@ public class KillrateConstant {
 	public static Map<String, String> allGameTypeMap;
 	static {
 		allGameTypeMap = new HashMap<String, String>();
-		allGameTypeMap.put("tls60","ssc");
-		allGameTypeMap.put("tls90","ssc");
-		allGameTypeMap.put("tls30","ssc");
-		allGameTypeMap.put("tls90a","ssc");
-		allGameTypeMap.put("tls90b","ssc");
-		allGameTypeMap.put("tls90c","ssc");
-		allGameTypeMap.put("tls90d","ssc");
+		allGameTypeMap.put("t1s60","ssc");
+		allGameTypeMap.put("t1s90","ssc");
+		allGameTypeMap.put("t1s30","ssc");
+		allGameTypeMap.put("t1s90a","ssc");
+		allGameTypeMap.put("t1s90b","ssc");
+		allGameTypeMap.put("t1s90c","ssc");
+		allGameTypeMap.put("t1s90d","ssc");
 		allGameTypeMap.put("t2s30","ssc");
 		allGameTypeMap.put("t3s90","ssc");
 		allGameTypeMap.put("t2s90","ssc");
