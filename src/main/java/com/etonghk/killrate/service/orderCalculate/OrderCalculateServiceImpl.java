df473f3ff04cac3002adfaa81f20df12798a80d4
ami.tsai <amitsai@etonghk.com>
2019-01-24 13:22:52 +0800

    調整計算

--- src/main/java/com/etonghk/killrate/service/orderCalculate/OrderCalculateServiceImpl.java
+++ src/main/java/com/etonghk/killrate/service/orderCalculate/OrderCalculateServiceImpl.java
@@ -8,17 +8,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
 
 import com.etonghk.killrate.awardNmber.AwardNumber;
 import com.etonghk.killrate.awardNmber.AwardNumberFactory;
 import com.etonghk.killrate.awardNmber.utils.AwardNumberUtil;
 import com.etonghk.killrate.awardSample.cache.AwardSampleCache;
-import com.etonghk.killrate.cache.RedisCache;
 import com.jack.entity.GameLotteryOrder;
 import com.jack.pool.DataFactory;
 
@@ -38,8 +33,6 @@ public class OrderCalculateServiceImpl implements OrderCalculateService{
 	@Autowired
 	private DataFactory dataFactory;
 	
-	@Autowired
-	private RedisCache redisCache;
 	/**
 	 * 	計算注單個開獎號碼金額
 	 */
@@ -48,25 +41,6 @@ public class OrderCalculateServiceImpl implements OrderCalculateService{
 		AwardNumber awardNumber = awardNumberFactory.getAwardNumber(order.getMethod());
 		Map<String,List<String>> typeByAwardNumber = awardNumber.getAwardNumberWithType(order);
 		Map<String,BigDecimal> resultToSet = AwardNumberUtil.getCalcAwardMoney(order, typeByAwardNumber, awardSampleCache, dataFactory);
-		
-		//取出key序列化元件
-		RedisSerializer<String> redisSerializer = redisCache.getRedisKeySerializer();
-		//要處理的key
-		byte[] key = redisSerializer.serialize(order.getLottery() + ":" + order.getIssue());
-		//撰寫RedisCallBack物件
-		RedisCallback<Void> pipelineCallback = new RedisCallback<Void>() {
-			//每一個pipeline需要實作自己準備批量放入的方式
-			@Override
-			public Void doInRedis(RedisConnection connection) throws DataAccessException {
-				//迴圈裝入物件
-				resultToSet.entrySet().forEach(entry->{
-					byte[] field = redisSerializer.serialize(entry.getKey());
-					connection.hIncrBy(key, field, entry.getValue().doubleValue());
-				});
-				return null;
-			}
-		};
-		redisCache.excutePipeline(pipelineCallback);
 		return resultToSet;
 	}
 
