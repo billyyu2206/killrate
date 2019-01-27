fee2aef5166afea9112a196c53c9685e2b3d272a
ami.tsai <amitsai@etonghk.com>
2019-01-27 13:35:10 +0800

    調整產獎期邏輯

--- src/main/java/com/etonghk/killrate/service/gameIssus/GameIssueServiceImpl.java
+++ src/main/java/com/etonghk/killrate/service/gameIssus/GameIssueServiceImpl.java
@@ -37,15 +37,14 @@ public class GameIssueServiceImpl implements GameIssueService{
 		
 		List<GamePeriod> gamelist = gamePeriodDao.getGamePeriodsList();
 		
-		LocalDateTime startTime = LocalDateTime.now().with(LocalTime.MIN);
-		LocalDateTime issueTime = startTime;
+		LocalDateTime nowStartTime = LocalDateTime.now().plusDays(afterDay).with(LocalTime.MIN);
 		for (GamePeriod gamePeriod : gamelist) {
-		
+			LocalDateTime issueStartTime = nowStartTime;
+			
 			String lottery = gamePeriod.getLottery();
 			int totoalPeriod = gamePeriod.getTotalPeriod();
 			int openSeconds =  gamePeriod.getOpenSeconds();
 			int periodSeconds = gamePeriod.getPeriodSeconds();
-			
 			// 產X天後獎期
 			List<GameIssue> gameIssueList = new ArrayList<GameIssue>();
 			GameIssue issue = null;
@@ -54,16 +53,16 @@ public class GameIssueServiceImpl implements GameIssueService{
 				issue = new GameIssue();
 				issue.setPlayId(KillrateConstant.allGameTypeMap.get(lottery));
 				issue.setLottery(lottery);
-				issue.setIssueDate(issueTime);
+				issue.setIssueDate(nowStartTime);
 				issue.setIssue(StringUtils.leftPad(i+"", 4, "0"));
-				String dateStr= startTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+				String dateStr= nowStartTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 				issue.setFullIssue(dateStr+"-"+ issue.getIssue());
-				issue.setIssueStartTime(issueTime);
-				LocalDateTime endTime = startTime.plusSeconds(periodSeconds);
-				issue.setIssueEndTime(endTime);
-				issue.setIssueOpenTime(endTime.plusSeconds(openSeconds));				
+				issue.setIssueStartTime(issueStartTime);
+				LocalDateTime issueEndTime = issueStartTime.plusSeconds(periodSeconds);
+				issue.setIssueEndTime(issueEndTime);
+				issue.setIssueOpenTime(issueEndTime.plusSeconds(openSeconds));				
 				gameIssueList.add(issue);
-				startTime = endTime;
+				issueStartTime = issueEndTime;
 			}
 			gameIssueDao.batchInsert(gameIssueList, gamePeriod.getLottery());
 		}
