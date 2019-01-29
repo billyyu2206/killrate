9340c7e75563e84b9ce06da191af33a6f57aa64d
PeterHong <peter5645@hotmail.com>
2019-01-29 09:46:22 +0800

    修改redis key 新增 結算請求key

--- src/main/java/com/etonghk/killrate/mq/sender/ClearKillRateSender.java
+++ src/main/java/com/etonghk/killrate/mq/sender/ClearKillRateSender.java
@@ -6,6 +6,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.etonghk.killrate.cache.RedisCache;
+import com.etonghk.killrate.cache.key.RedisKey;
 import com.etonghk.killrate.mq.config.ClearRateMqConfig;
 import com.etonghk.killrate.vo.ClearKillRateVo;
 
@@ -21,13 +23,22 @@ public class ClearKillRateSender {
 	@Autowired
     private RabbitTemplate rabbitTemplate;
     
+	@Autowired
+	private RedisCache cache;
+	
     /**
             * 寄送Order訂單
      * @param order
      */
     public void sendClearLotteryIssue(ClearKillRateVo clearKillRateVo) {
     	logger.info("clear lottery={},issue={} => ",clearKillRateVo.getLottery(),clearKillRateVo.getIssue());
-    	rabbitTemplate.convertAndSend(ClearRateMqConfig.CLEAR_RATE_EXCHANGE,"", clearKillRateVo);
+    	String clearKey = RedisKey.getLotteryIssueClearKey(clearKillRateVo.getLottery(), clearKillRateVo.getIssue());
+    	if(cache.getObj(clearKey)==null) {
+    		rabbitTemplate.convertAndSend(ClearRateMqConfig.CLEAR_RATE_EXCHANGE,"", clearKillRateVo);
+    		cache.putObj(clearKey, "1");
+    	}else {
+    		return;
+    	}
     }
 	
 }
